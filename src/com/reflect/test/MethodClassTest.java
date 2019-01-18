3e67feebdcb43cd9034a842e88e77931ddc5132b
zhangbiao <18500199974@163.com>
2019-01-18 15:18:23 +0800

    通过Class来获取实例对象中的方法返回类型，方法名，参数列表

---
+++ src/com/reflect/test/MethodClassTest.java
@@ -0,0 +1,51 @@
+package com.reflect.test;
+
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+/**
+ * 〈功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2019/1/18
+ */
+public class MethodClassTest {
+
+
+    @Test
+    public void test01() {
+        String str = "haha";
+        printClassMessage(str);
+    }
+
+
+    /**
+     * 获取类中的所有方法
+     */
+    public static void printClassMessage(Object obj) {
+        Class c1 = obj.getClass();
+        System.out.println("类的名称是：" + c1.getName());
+        //获取修饰符为public的方法
+        Method[] methods = c1.getMethods();
+        //获取所有的方法（包括private,protected）
+        //Method[] declaredMethods = c1.getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            //获取返回类型的类类型
+            Class returnType = methods[i].getReturnType();
+            System.out.print(returnType.getName() + "\t");
+            //获取方法名称
+            String methodName = methods[i].getName();
+            System.out.print(methodName + "(");
+            //获取每个方法中的参数类型，
+            Class[] parameterTypes = methods[i].getParameterTypes();
+            for (int j = 0; j < parameterTypes.length; j++) {
+                //参数类型名称（不包括包名）
+                System.out.print(parameterTypes[j].getSimpleName() + ",");
+            }
+            System.out.print(")");
+            System.out.println();
+        }
+    }
+}
